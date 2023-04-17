9a965e91126f57cbefdb9e8d0a4632c420630ac1
Ramiro <ramiro.escalante@bue.edu.ar>
2023-04-17 20:06:07 -0300

    Clase 13. JS 1. Introducción a JS.

---
+++ js-1/funciones-aritmeticas.js
@@ -0,0 +1,39 @@
+num1 = 10;
+var num2;
+// int (number)
+num2 = 12;
+console.log(typeof num2);
+var nombreApellido = "Ramiro Escalante Leiva";  // let
+var nombre_apellido = "Ramiro Escalante Leiva";
+console.log(typeof nombre_apellido);
+// float (number)
+decimal = 12.3;
+console.log(typeof decimal);
+decimal = "12.3";
+console.log(typeof decimal);
+logico = true  // false
+console.log(typeof logico);
+
+// Funciones aritméticas
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % 3);  // 1
+console.log(num1 ** 2);
+console.log(num1 ** (1/3));  // raiz
+
+// constantes
+const VALOR_PI = 3.14159
+
+console.clear()
+i = 0;
+console.log(i);
+i++;  // i = i + 1;
+console.log(i);
+i++;  // i = i + 1;
+console.log(i);
+i++;  // i = i + 1;
+console.log(i);
+i++;  // i = i + 1;
+console.log(i);
---
+++ js-1/js/prompt.js
@@ -0,0 +1,5 @@
+console.log("Ingrese los datos del usuario: ");
+nombre = prompt("Ingrese el nombre: ");
+edad = parseInt(prompt("Ingrese la edad: "));
+console.log("Mi nombre es",nombre, "y mi edad es", edad);
+console.log("Mi nombre es" + nombre + "y mi edad es" + edad);
